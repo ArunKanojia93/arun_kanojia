21aa5b8c6444d0d560c774f717805a48da8e936c
ArunKanojia93 <arun@pyzalabs.com>
2024-09-22 00:27:07 +0530

    fead: added pdf viewer and resume download

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { Button } from "@mantine/core";
-import { IconDownload } from "@tabler/icons-react";
+import { ActionIcon, Button, Modal, ScrollArea, Tooltip } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconArrowBigDownLines, IconArrowBigDownLinesFilled } from "@tabler/icons-react";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import TypeWriter from "typewriter-effect";
 import { NeonGradientCard } from "./MagicUi/NeonGradientCard";
@@ -10,44 +12,72 @@ import SparklesText from "./MagicUi/SparkelsText";
 import Info from "./User";
 
 const About = () => {
+  const PdfViewer = dynamic(() => import("./PDFViewer/PDFViewer"), { ssr: false });
+  const [opened, { open, close }] = useDisclosure(false);
   return (
-    <div id="about" className="lg:h-[80vh] w-full flex justify-center">
-      <Particles quantity={500} size={1} staticity={20} className="pointer-events-none absolute w-full h-full left-0 top-0" />
-      <div className="max-w-screen-2xl w-full h-full flex flex-col-reverse lg:flex-row py-10 px-8 md:px-16 xl:px-32 3xl:px-0 justify-between items-center gap-y-16">
-        <div className="flex flex-col w-full lg:w-3/5 items-center lg:items-start">
-          <div className="text-3xl xl:text-4xl text-primary">Hi, I am</div>
-          <SparklesText text={Info.name} className="text-4xl xl:text-7xl font-black text-white !leading-normal" />
-          <div className="text-white text-3xl xl:text-4xl flex">
-            My Stack:{" "}
-            <span className="text-primary font-semibold">
-              <TypeWriter
-                options={{
-                  strings: Info.stack,
-                  autoStart: true,
-                  loop: true,
-                  deleteSpeed: 20,
-                  cursor: "_",
-                  delay: 100,
-                }}
-              />
-            </span>
-          </div>
-          <div className="text-lg xl:text-xl font-semibold text-justify my-10 w-full md:w-11/12">{Info.bio}</div>
+    <>
+      <div id="about" className="lg:h-[80vh] w-full flex justify-center">
+        <Particles quantity={500} size={1} staticity={20} className="pointer-events-none absolute w-full h-full left-0 top-0" />
+        <div className="max-w-screen-2xl w-full h-full flex flex-col-reverse lg:flex-row py-10 px-8 md:px-16 xl:px-32 3xl:px-0 justify-between items-center gap-y-16">
+          <div className="flex flex-col w-full lg:w-3/5 items-center lg:items-start">
+            <div className="text-3xl xl:text-4xl text-primary">Hi, I am</div>
+            <SparklesText text={Info.name} className="text-4xl xl:text-7xl font-black text-white !leading-normal" />
+            <div className="text-white text-3xl xl:text-4xl flex">
+              My Stack:{" "}
+              <span className="text-primary font-semibold">
+                <TypeWriter
+                  options={{
+                    strings: Info.stack,
+                    autoStart: true,
+                    loop: true,
+                    deleteSpeed: 20,
+                    cursor: "_",
+                    delay: 100,
+                  }}
+                />
+              </span>
+            </div>
+            <div className="md:text-lg xl:text-xl font-semibold text-justify my-10 w-full md:w-11/12">{Info.bio}</div>
 
-          <div className="flex gap-4">
-            <Button size="md" radius="md" className="bg-foreground text-background w-fit hover:bg-foreground hover:text-background hover:shadow-[0px_0px_4px_2px] hover:shadow-primary transition-shadow duration-300">
-              Check Resume
-            </Button>
-            <Button size="md" radius="md" className="bg-background text-foreground w-fit border-2 border-primary hover:bg-background hover:text-foreground hover:shadow-[0px_0px_4px_2px] hover:shadow-primary transition-shadow duration-300">
-              Download <IconDownload size={20} className="ml-2 text-primary" />
-            </Button>
+            <div className="flex gap-4">
+              <Button onClick={open} size="md" radius="md" className="bg-foreground text-background w-fit hover:bg-foreground hover:text-background hover:shadow-[0px_0px_4px_2px] hover:shadow-primary transition-shadow duration-300">
+                Check Resume
+              </Button>
+              <Button
+                component="a"
+                href="/assets/arun@pyzalabs.com.pdf"
+                download={Info.email}
+                size="md"
+                radius="md"
+                className="bg-background text-foreground w-fit border-2 border-primary hover:bg-background hover:text-foreground hover:shadow-[0px_0px_4px_2px] hover:shadow-primary transition-shadow duration-300"
+              >
+                Download <IconArrowBigDownLines size={20} className="ml-2 text-foreground animate-bounce" />
+              </Button>
+            </div>
           </div>
+          <NeonGradientCard className="max-w-40 max-h-40 md:max-w-80 md:max-h-80 xl:max-w-sm xl:max-h-fit items-center justify-center">
+            <Image src="/assets/dp.png" alt="profile" className="pointer-events-none z-10 rounded-full bg-gradient-to-br from-[#ff2975] from-35% to-[#00FFF1]" width={384} height={384} />
+          </NeonGradientCard>
         </div>
-        <NeonGradientCard className="max-w-80 h-80 xl:max-w-sm xl:h-full items-center justify-center">
-          <Image src="/assets/dp.png" alt="profile" className="pointer-events-none z-10 rounded-full bg-gradient-to-br from-[#ff2975] from-35% to-[#00FFF1]" width={384} height={384} />
-        </NeonGradientCard>
       </div>
-    </div>
+      <Modal.Root scrollAreaComponent={ScrollArea.Autosize} opened={opened} onClose={close} size="auto" centered>
+        <Modal.Overlay />
+        <Modal.Content className="rounded-2xl border border-primary shadow-[0px_0px_4px_1px] shadow-primary">
+          <Modal.Header className="bg-background">
+            <Modal.Title className="text-3xl font-bold">Resume</Modal.Title>
+            <Tooltip label="Download" position="right" offset={10} className="bg-foreground text-background">
+              <ActionIcon component="a" href="/assets/arun@pyzalabs.com.pdf" download={Info.email} variant="outline" aria-label="Settings" className="ml-4 border-foreground">
+                <IconArrowBigDownLinesFilled stroke={1.5} className="text-foreground animate-bounce" />
+              </ActionIcon>
+            </Tooltip>
+            <Modal.CloseButton size={32} className="text-foreground hover:bg-background hover:text-primary" />
+          </Modal.Header>
+          <Modal.Body>
+            <PdfViewer />
+          </Modal.Body>
+        </Modal.Content>
+      </Modal.Root>
+    </>
   );
 };
 
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,20 +7,23 @@ const Experience = () => {
     <div id="experience" className="flex flex-col pt-20 px-8 md:px-16 xl:px-32 3xl:px-0 items-center">
       <div className="text-5xl font-bold text-primary pb-20">Experience</div>
       <Timeline active={2} color="#DF73FF" className="w-full max-w-screen-2xl" bulletSize={32}>
-        {Info.experience.map(({ position, period, description, logo, company }, i) => (
-          <Timeline.Item data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="300" key={i} bullet={<IconBriefcaseFilled size={32} className="text-background" />} className="text-primary text-4xl font-semibold pt-12">
+        {Info.experience.map(({ position, period, description, logo, company }) => (
+          <Timeline.Item data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="300" key={company} bullet={<IconBriefcaseFilled size={32} className="text-background" />} className="text-primary text-2xl md:text-4xl font-semibold pt-12">
             <div className="bg-gradient-to-r from-purple-500 to-pink-500 p-0.5 rounded-2xl shadow-[0px_0px_4px_1px] shadow-primary">
               <div className="bg-background p-4 rounded-2xl">
-                <div className="flex items-center gap-4 pt-4">
+                <div className="flex flex-col sm:flex-row items-center gap-4 pt-4">
                   {logo}
-                  <div className="text-foreground text-3xl flex flex-col">
+                  <div className="text-foreground text-xl md:text-3xl text-center sm:text-left flex flex-col">
                     {position}
-                    <span className="flex items-center gap-2 text-base text-gray-400">
-                      {company} <IconPointFilled size={12} /> {period}
+                    <span className="flex flex-col sm:flex-row items-center gap-2 text-sm md:text-base text-gray-400">
+                      <span className="flex items-end gap-2">
+                        {company} <IconPointFilled size={12} />
+                      </span>{" "}
+                      {period}
                     </span>
                   </div>
                 </div>
-                <div className="text-foreground text-base py-4 text-justify font-sans">{description}</div>
+                <div className="text-foreground text-sm md:text-base py-4 text-justify font-sans">{description}</div>
               </div>
             </div>
           </Timeline.Item>
