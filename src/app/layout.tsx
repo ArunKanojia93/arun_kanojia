3c84408d376fefd9bc50b62c76f5cb0817a1043a
ArunKanojia93 <arun@pyzalabs.com>
2024-10-12 15:44:06 +0530

    add og tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "@mantine/core/styles.css";
 import "aos/dist/aos.css";
-import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -18,11 +17,6 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export const metadata: Metadata = {
-  title: "Arun Kanojia",
-  description: "Frontend engineer",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -31,6 +25,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>Arun Kanojia</title>
+        <meta name="description" content="Frontend engineer" />
+
+        <meta property="og:url" content="https://arun-kanojia.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Arun Kanojia" />
+        <meta property="og:description" content="Frontend engineer" />
+        <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/e46ec848-6d26-4b40-9ebb-d4cca119d973.png?token=Z0twFNmzMlDXoVmwO_jwkmulp6EOhi1T1vV4E1Zkosk&height=1105&width=1102&expires=33264724839" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="arun-kanojia.vercel.app" />
+        <meta property="twitter:url" content="https://arun-kanojia.vercel.app" />
+        <meta name="twitter:title" content="Arun Kanojia" />
+        <meta name="twitter:description" content="Frontend engineer" />
+        <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/e46ec848-6d26-4b40-9ebb-d4cca119d973.png?token=Z0twFNmzMlDXoVmwO_jwkmulp6EOhi1T1vV4E1Zkosk&height=1105&width=1102&expires=33264724839" />
         <ColorSchemeScript />
         <AOSInit />
       </head>
